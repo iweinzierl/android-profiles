878add1e44fa9d59e21904331b6b15be3daf5908
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2015-09-13 20:28:54 +0200

    Improved the layout of lists.

--- app/src/main/java/de/iweinzierl/easyprofiles/adapter/TriggerAdapter.java
+++ app/src/main/java/de/iweinzierl/easyprofiles/adapter/TriggerAdapter.java
@@ -1,9 +1,11 @@
 package de.iweinzierl.easyprofiles.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -27,11 +29,16 @@ public class TriggerAdapter extends ListAdapter<Trigger> {
 
         view = layoutInflater.inflate(R.layout.list_item_trigger, viewGroup, false);
 
-        TextView triggerType = (TextView) view.findViewById(R.id.trigger_type);
+        ImageView triggerType = (ImageView) view.findViewById(R.id.trigger_type);
         TextView triggerDetails = (TextView) view.findViewById(R.id.trigger_details);
         TextView profileName = (TextView) view.findViewById(R.id.profile);
 
-        triggerType.setText(trigger.getType().name());
+        Resources resources = context.getResources();
+        Resources.Theme theme = context.getTheme();
+        switch (trigger.getType()) {
+            case WIFI:
+                triggerType.setImageDrawable(resources.getDrawable(R.drawable.ic_network_wifi_black_48px, theme));
+        }
         triggerDetails.setText(trigger.getData());
         profileName.setText(profile.getName());
 
