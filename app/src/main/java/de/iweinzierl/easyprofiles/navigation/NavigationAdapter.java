36412dc8d3f9dcdff44aa5668a017768245612c5
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2015-09-14 20:18:28 +0200

    Proper layout with icons for the nav drawer.

--- app/src/main/java/de/iweinzierl/easyprofiles/navigation/NavigationAdapter.java
+++ app/src/main/java/de/iweinzierl/easyprofiles/navigation/NavigationAdapter.java
@@ -1,9 +1,11 @@
 package de.iweinzierl.easyprofiles.navigation;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.common.collect.Lists;
@@ -28,11 +30,24 @@ public class NavigationAdapter extends ListAdapter<Integer> {
     public View getView(int i, View view, ViewGroup viewGroup) {
         int navItem = (int) getItem(i);
 
+        Resources res = context.getResources();
+        Resources.Theme theme = context.getTheme();
+
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(R.layout.list_item_navigation, null);
 
         TextView tv = (TextView) view.findViewById(R.id.title);
         tv.setText(navItem);
+
+        ImageView iv = (ImageView) view.findViewById(R.id.icon);
+        switch (navItem) {
+            case R.string.activity_triggerlist:
+                iv.setImageDrawable(res.getDrawable(R.drawable.ic_schedule_black_36dp, theme));
+                break;
+            case R.string.activity_profilelist:
+                iv.setImageDrawable(res.getDrawable(R.drawable.ic_group_black_36dp, theme));
+                break;
+        }
 
         return view;
     }
