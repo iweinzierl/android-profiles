c2bda60cf4c3e9fc3a4981d6c979ebb66fe23b4a
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2015-10-08 22:46:33 +0200

    Properly formatted display for time based triggers in the trigger list.

--- app/src/main/java/de/iweinzierl/easyprofiles/adapter/TriggerAdapter.java
+++ app/src/main/java/de/iweinzierl/easyprofiles/adapter/TriggerAdapter.java
@@ -2,6 +2,7 @@ package de.iweinzierl.easyprofiles.adapter;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,11 @@ import android.widget.TextView;
 import com.orm.SugarRecord;
 
 import java.util.List;
+import java.util.Set;
 
 import de.iweinzierl.easyprofiles.R;
+import de.iweinzierl.easyprofiles.domain.Day;
+import de.iweinzierl.easyprofiles.domain.TimeBasedTrigger;
 import de.iweinzierl.easyprofiles.persistence.PersistentTrigger;
 import de.iweinzierl.easyprofiles.persistence.Profile;
 
@@ -36,11 +40,77 @@ public class TriggerAdapter extends ListAdapter<PersistentTrigger> {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         final PersistentTrigger persistentTrigger = (PersistentTrigger) getItem(i);
-        final Profile profile = SugarRecord.findById(Profile.class, persistentTrigger.getOnActivateProfileId());
 
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        view = layoutInflater.inflate(R.layout.list_item_trigger, viewGroup, false);
+        switch (persistentTrigger.getType()) {
+            case WIFI:
+                return buildWifiView(persistentTrigger, layoutInflater, viewGroup);
+            case TIME_BASED:
+                return buildTimeBasedView(persistentTrigger, layoutInflater, viewGroup);
+            default:
+                return new View(context);
+        }
+    }
+
+    private View buildTimeBasedView(final PersistentTrigger persistentTrigger, final LayoutInflater layoutInflater, final ViewGroup viewGroup) {
+        TimeBasedTrigger trigger = new TimeBasedTrigger();
+        trigger.apply(persistentTrigger);
+
+        Resources resources = context.getResources();
+        Resources.Theme theme = context.getTheme();
+
+        View view = layoutInflater.inflate(R.layout.list_item_time_based_trigger, viewGroup, false);
+
+        Switch enabled = (Switch) view.findViewById(R.id.enabled);
+        TextView timeActivation = (TextView) view.findViewById(R.id.time_activation);
+        TextView timeDeactivation = (TextView) view.findViewById(R.id.time_deactivation);
+        TextView profileActivation = (TextView) view.findViewById(R.id.profile_activation);
+        TextView profileDeactivation = (TextView) view.findViewById(R.id.profile_deactivation);
+
+        enabled.setChecked(persistentTrigger.isEnabled());
+        timeActivation.setText(trigger.getActivationTime().toString("HH:mm"));
+        profileActivation.setText(trigger.getOnActivateProfile().getName());
+
+        if (trigger.getDeactivationTime() != null && trigger.getOnDeactivateProfile() != null) {
+            timeDeactivation.setText(trigger.getDeactivationTime().toString("HH:mm"));
+            profileDeactivation.setText(trigger.getOnDeactivateProfile().getName());
+        }
+
+        enabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    callback.onTriggerEnabled(persistentTrigger);
+                } else {
+                    callback.onTriggerDisabled(persistentTrigger);
+                }
+            }
+        });
+
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.MONDAY, (TextView) view.findViewById(R.id.monday));
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.TUESDAY, (TextView) view.findViewById(R.id.tuesday));
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.WEDNESDAY, (TextView) view.findViewById(R.id.wednesday));
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.THURSDAY, (TextView) view.findViewById(R.id.thursday));
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.FRIDAY, (TextView) view.findViewById(R.id.friday));
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.SATURDAY, (TextView) view.findViewById(R.id.saturday));
+        markDayIfActivatedForRepeat(trigger.getRepeatOnDays(), Day.SUNDAY, (TextView) view.findViewById(R.id.sunday));
+
+        return view;
+    }
+
+    private void markDayIfActivatedForRepeat(Set<Day> repeatDays, Day day, TextView dayView) {
+        if (repeatDays != null &&    repeatDays.contains(day)) {
+            Resources resources = context.getResources();
+            dayView.setTextColor(resources.getColor(R.color.ColorPrimary));
+            dayView.setTypeface(null, Typeface.BOLD);
+        }
+    }
+
+    private View buildWifiView(final PersistentTrigger persistentTrigger, final LayoutInflater layoutInflater, final ViewGroup viewGroup) {
+        final Profile profile = SugarRecord.findById(Profile.class, persistentTrigger.getOnActivateProfileId());
+
+        View view = layoutInflater.inflate(R.layout.list_item_trigger, viewGroup, false);
 
         ImageView triggerType = (ImageView) view.findViewById(R.id.trigger_type);
         TextView triggerDetails = (TextView) view.findViewById(R.id.trigger_details);
@@ -49,15 +119,11 @@ public class TriggerAdapter extends ListAdapter<PersistentTrigger> {
 
         Resources resources = context.getResources();
         Resources.Theme theme = context.getTheme();
-        switch (persistentTrigger.getType()) {
-            case WIFI:
-                triggerType.setImageDrawable(resources.getDrawable(R.drawable.ic_network_wifi_black_48px, theme));
-            case TIME_BASED:
-                triggerType.setImageDrawable(resources.getDrawable(R.drawable.time_schedule_36px, theme));
-        }
+
         triggerDetails.setText(persistentTrigger.getData());
         profileName.setText(profile.getName());
         enabled.setChecked(persistentTrigger.isEnabled());
+        triggerType.setImageDrawable(resources.getDrawable(R.drawable.ic_network_wifi_black_48px, theme));
 
         enabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
